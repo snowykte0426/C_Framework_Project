955da78d2179eaa89875a989a9df0503e3525a5b
snowykte0426 <snowykte0426@naver.com>
2024-05-03 23:57:44 +0900

    HTTP 서버 코드 완성 및 index.htm 추가

--- C_Lang_Project_Back_Server/main.c
+++ C_Lang_Project_Back_Server/main.c
@@ -1,88 +1,122 @@
-#define _WINSOCK_DEPRECATED_NO_WARNINGS
+ï»¿#define _WINSOCK_DEPRECATED_NO_WARNINGS
 #define _CRT_SECURE_NO_WARNINGS
-
-#include <winsock.h> // socket Çì´õÆÄÀÏ  Ãß°¡
+#include <winsock2.h>
 #include <stdio.h>
 #include <stdlib.h>
-#pragma comment(lib, "ws2_32")  //socket Åë½Å ¶óÀÌºê·¯¸® Ãß°¡
-
+#include <windows.h>
+#include <time.h>
+#pragma comment(lib, "ws2_32")
+void WelcomeMessage() {
+    SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), 10);
+    printf(" _____   _                            _   _         _         \n"
+        "/  __ \\ | |                          | \\ | |       | |        \n"
+        "| /  \\/ | |      __ _  _ __    __ _  |  \\| |  ___  | |_   ___ \n"
+        "| |     | |     / _` || '_ \\  / _` | | . ` | / _ \\ | __| / _ \\\n"
+        "| \\__/\\ | |____| (_| || | | || (_| | | |\\  || (_) || |_ |  __/\n"
+        " \\____/ \\_____/ \\__,_||_| |_| \\__, | \\_| \\_/ \\___/  \\__| \\___|\n"
+        "                               __/ |                          \n"
+        "                              |___/                           \n");
+    printf(
+        "_______ _______  ______   _    _ _______  ______       _______ _____  __   _  ______ _______        _______\n"
+        "|______ |______ |_____ /   \\  /  |______ |_____/       |      |     | | \\  | |______ |     | |      |______\n"
+        "______| |______ |      \\_   \\/   |______ |     \\_      |_____ |_____| |  \\_| ______| |_____| |_____ |______\n\n\n\n");
+    SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), 15);
+}
 #define BUFSIZE 512
+#define HTMFILE "index.htm"
+SOCKET listen_sock;
+DWORD WINAPI ListenThread(LPVOID arg) {
+    time_t timer = time(NULL);
+    SOCKADDR_IN clientaddr;
+    int addrlen;
+    SOCKET client_sock;
+    char buf[BUFSIZE + 1];
+    int retval;
+    while (1) {
+        addrlen = sizeof(clientaddr);
+        client_sock = accept(listen_sock, (SOCKADDR*)&clientaddr, &addrlen);
+        if (client_sock == INVALID_SOCKET) {
+            printf("accept() ì‹¤í–‰ ì—ëŸ¬\n");
+            continue;
+        }
+        printf("--------------------------------------\nìš”ì²­ ìˆ˜ì‹ \n--------------------------------------\n");
+        retval = recv(client_sock, buf, BUFSIZE, 0);
+        if (retval == SOCKET_ERROR) {
+            printf("recv() ì‹¤í–‰ ì—ëŸ¬\n");
+            closesocket(client_sock);
+            continue;
+        }
+        struct tm* user_time;
+        timer = time(NULL);
+        user_time = localtime(&timer);
+        buf[retval] = '\0';
+        printf("<%d:%d:%d>[TCP/%s:%d]\n%s\n",user_time->tm_hour,user_time->tm_min,user_time->tm_sec, inet_ntoa(clientaddr.sin_addr), ntohs(clientaddr.sin_port), buf);
+        if (strstr(buf, "GET / ") || strstr(buf, "GET /index.htm ")) {
+            FILE* file = fopen(HTMFILE, "r");
+            if (file) {
+                fseek(file, 0, SEEK_END);
+                long filesize = ftell(file);
+                fseek(file, 0, SEEK_SET);
+                char* filebuffer = (char*)malloc(filesize + 1);
+                fread(filebuffer, filesize, 1, file);
+                filebuffer[filesize] = '\0';
+                char header[1024];
+                sprintf(header, "HTTP/1.1 200 OK\r\nContent-Type: text/html\r\nContent-Length: %ld\r\nConnection: close\r\n\r\n", filesize);
+                send(client_sock, header, strlen(header), 0);
+                send(client_sock, filebuffer, filesize, 0);
+                free(filebuffer);
+                fclose(file);
+            }
+            else {
+                char response[] = "HTTP/1.1 404 Not Found\r\nContent-Type: text/html\r\nConnection: close\r\n\r\n<html><body><h1>404 Not Found</h1></body></html>";
+                send(client_sock, response, strlen(response), 0);
+            }
+        }
+        else {
+            char response[] = "HTTP/1.1 400 Bad Request\r\nContent-Type: text/html\r\nConnection: close\r\n\r\n<html><body><h1>400 Bad Request</h1></body></html>";
+            send(client_sock, response, strlen(response), 0);
+        }
+        closesocket(client_sock);
+    }
+    return 0;
+}
 int main() {
-	int retval;   // À©¼Ó ÃÊ±âÈ­
-	WSADATA wsa;  // À©¼Ó Á¤º¸¸¦ ´ãÀ» ±¸Á¶Ã¼ 
-	// À©¼Ó ¹öÀü 2.2¸¦ »ç¿ëÇÒ ¼ö ÀÖ´ÂÁö OS¿¡ È®ÀÎ
-	// »ç¿ë°¡´ÉÇÏ¸é wsa¿¡ °ªÀ» Ã¤¿öÁÜ
-	// WSAStartup ÇÔ¼ö´Â ws2_32.dll »ç¿ë ½ÃÀÛ
+    WelcomeMessage();
+    WSADATA wsa;
+    if (WSAStartup(MAKEWORD(2, 2), &wsa) != 0)
+        return -1;
 
-	if (WSAStartup(MAKEWORD(2, 2), &wsa) != 0)
-		return -1;
-
-	//------------------socket() »ç¿ë-----------------------
-	//¼­¹ö°¡ client ¿¬°á ¼ö½Å ´ë±â
-
-	SOCKET listen_sock = socket(AF_INET, SOCK_STREAM, 0);
-
-	//IPv4, TCP/IPÅë½Å
-
-	if (listen_sock == INVALID_SOCKET) {
-		printf("¼ÒÄÏ »ı¼º ½ÇÆĞ, ¿¡·¯ÄÚµå: %d\n", WSAGetLastError());
-		WSACleanup();     //À©¼Ó ÇØÁ¦
-		return 0;
-	}
-	printf("¼ÒÄÏÀÌ »ı¼ºµÇ¾ú½À´Ï´Ù.\n");
-
-	//----------------------bind()»ç¿ë---------------------------
-	//Å¬¶óÀÌ¾ğÆ®°¡ ¼­¹öÀÇ À§Ä¡¸¦ ¾Ë±â À§ÇÑ IP¿Í Port µî·Ï ÀÛ¾÷
-
-	SOCKADDR_IN serveraddr;   //socket ÁÖ¼Ò¿Í port¸¦ ÇÒ´çÇÏ±â À§ÇØ
-	//SOCKETADDR_IN ±¸Á¶Ã¼ »ç¿ë
-	ZeroMemory(&serveraddr, sizeof(serveraddr));
-	serveraddr.sin_family = AF_INET;    //IPv4 ÁÖ¼Ò »ç¿ë
-
-	//ÀÌ ÄÄÇ»ÅÍ¿¡ Á¸ÀçÇÏ´Â ·£Ä«µå Áß »ç¿ë °¡´ÉÇÑ °É ÀÚµ¿ ¼±ÅÃ
-	serveraddr.sin_addr.s_addr = htonl(INADDR_ANY);
-	serveraddr.sin_port = htons(9000);
-
-	if (bind(listen_sock, (SOCKADDR*)&serveraddr, sizeof(serveraddr)) == -1) {
-		printf("bind() ½ÇÇà ¿¡·¯ \n");
-		exit(1);
-	}
-	else {
-		printf("bind ¼º°ø\n");
-	}
-	char check[10];
-	//---------------------------listen()-------------------------------
-	while (1) {
-		listen(listen_sock, SOMAXCONN); //SOMAXCONN: ¸Æ½Ã¸Ø Å¥ »çÀÌÁî
-		printf("¿¬°á ´ë±â Áß....");
-		//---------------------------------------------------------------
-		//Åë½Å¿¡ »ç¿ëÇÒ º¯¼ö
-		SOCKET client_sock;
-		SOCKADDR_IN clientaddr;
-		int addrlen;
-		char buf[BUFSIZE + 1];
-
-		//--------------------------accept()------------------------------
-		addrlen = sizeof(clientaddr);
-		client_sock = accept(listen_sock, (SOCKADDR*)&clientaddr, &addrlen);
-		printf("¿äÃ» ¼ö½Å\n");
-
-		//----------------------------recv()------------------------------
-		retval = recv(client_sock, buf, BUFSIZE, 0);
-		printf("¸Ş¼¼Áö ¼ö½Å\n");
-
-		//------------------------¹ŞÀº µ¥ÀÌÅÍ Ãâ·Â------------------------------
-		buf[retval] = '\0';
-		printf("[TCP/%s:%d] %s\n",
-			inet_ntoa(clientaddr.sin_addr),
-			ntohs(clientaddr.sin_port), buf);
-
-		//----------------------------close socket------------------------------
-		closesocket(listen_sock);
-
-		//----------------------------À©¼Ó Á¾·á----------------------------------
-	}
-	WSACleanup();
-
-	return 0;
+    listen_sock = socket(AF_INET, SOCK_STREAM, 0);
+    if (listen_sock == INVALID_SOCKET) {
+        printf("ì†Œì¼“ ìƒì„± ì‹¤íŒ¨, ì—ëŸ¬ì½”ë“œ: %d\n", WSAGetLastError());
+        WSACleanup();
+        return 0;
+    }
+    SOCKADDR_IN serveraddr;
+    ZeroMemory(&serveraddr, sizeof(serveraddr));
+    serveraddr.sin_family = AF_INET;
+    serveraddr.sin_addr.s_addr = htonl(INADDR_ANY);
+    serveraddr.sin_port = htons(9000);
+    if (bind(listen_sock, (SOCKADDR*)&serveraddr, sizeof(serveraddr)) == -1) {
+        printf("bind() ì‹¤í–‰ ì—ëŸ¬\n");
+        closesocket(listen_sock);
+        WSACleanup();
+        return 1;
+    }
+    if (listen(listen_sock, SOMAXCONN) == -1) {
+        printf("listen() ì‹¤í–‰ ì—ëŸ¬\n");
+        closesocket(listen_sock);
+        WSACleanup();
+        return 1;
+    }
+    printf("ì—°ê²° ëŒ€ê¸° ì¤‘....\n");
+    CreateThread(NULL, 0, ListenThread, NULL, 0, NULL);
+    char cmd[10];
+    do {
+        printf("ì„œë²„ë¥¼ ì¢…ë£Œí•˜ë ¤ë©´ 'exit'ì„ ì…ë ¥í•˜ì„¸ìš”.\n");
+        scanf("%s", cmd);
+    } while (strcmp(cmd, "exit") != 0);
+    closesocket(listen_sock);
+    WSACleanup();
+    return 0;
 }
